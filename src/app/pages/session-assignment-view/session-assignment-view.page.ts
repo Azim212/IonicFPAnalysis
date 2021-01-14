8a556e66219bbe477fd7dd4f76ee19e2f11843ad
Azim212 <44842575+Azim212@users.noreply.github.com>
2021-01-14 16:10:57 +0800

    Analysis Tab. Show status of brfv5

--- src/app/pages/session-assignment-view/session-assignment-view.page.ts
+++ src/app/pages/session-assignment-view/session-assignment-view.page.ts
@@ -1,13 +1,13 @@
 import { Component, OnInit, NgZone } from '@angular/core';
-import { ActivatedRoute,Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { AssignmentsService } from '../../services/assignments.service';
 import { ModalController, Platform } from '@ionic/angular';
 import { SessionAssignmentChatPage } from '../../pages/session-assignment-chat/session-assignment-chat.page';
 import { Storage } from '@ionic/storage';
-import {NetworkService} from '../../services/network.service';
-import {apiurl, SessionStatus} from '../../model/global-api';
+import { NetworkService } from '../../services/network.service';
+import { apiurl, SessionStatus } from '../../model/global-api';
 import { DomSanitizer } from '@angular/platform-browser';
-import { GlobalService} from '../../services/global.service';
+import { GlobalService } from '../../services/global.service';
 import { SessionViewImageDiscussionPage } from '../shared-module/session-view-image-discussion/session-view-image-discussion.page';
 import { SessionMyPage } from '../session-my/session-my.page';
 import { Observable } from 'rxjs/internal/Observable';
@@ -27,130 +27,123 @@ export class SessionAssignmentViewPage implements OnInit {
   refreshvalue: boolean = false;
   test3: any;
 
-  constructor(private ngZone:NgZone,private nativeHttp:HTTP,private globalService: GlobalService,private platform:Platform,private sessionmypage:SessionMyPage ,public dms:DomSanitizer,private router:Router,private activatedRoute: ActivatedRoute,private modalController:ModalController, private assignmentsService: AssignmentsService,private storage:Storage,private networkService:NetworkService) { }
+  constructor(private ngZone: NgZone, private nativeHttp: HTTP, private globalService: GlobalService, private platform: Platform, private sessionmypage: SessionMyPage, public dms: DomSanitizer, private router: Router, private activatedRoute: ActivatedRoute, private modalController: ModalController, private assignmentsService: AssignmentsService, private storage: Storage, private networkService: NetworkService) { }
 
   //Segment
-  segmentModel = "assignment";isactiveassignment:string; isactivevideoorvoice:string;asgmtid:any;sessionid:any;
- //User Info
- token:any;studentid:any;tutornamefordisplay:any;
-  asgmtlist:any;sessionstatusended:any;sessionstatus:any;imgdisplayinapp:any;asgmtdiscusslist:any;discussimgdisplayinapp:any;
-  ngOnInit() { 
+  segmentModel = "assignment"; isactiveassignment: string; isactivevideoorvoice: string; isactiveanalysis: string; asgmtid: any; sessionid: any;
+  //User Info
+  token: any; studentid: any; tutornamefordisplay: any;
+  asgmtlist: any; sessionstatusended: any; sessionstatus: any; imgdisplayinapp: any; asgmtdiscusslist: any; discussimgdisplayinapp: any;
+  ngOnInit() {
     //Segment
-    console.log('this.router.url','this.rout')
-  this.isactiveassignment="activesegment";
+    console.log('this.router.url', 'this.rout')
+    this.isactiveassignment = "activesegment";
   }
 
-  ionViewWillEnter()
-  {
-    this.token=this.globalService.getselectedtoken()
-    this.studentid=this.globalService.getselectedStudentId()
+  ionViewWillEnter() {
+    this.token = this.globalService.getselectedtoken()
+    this.studentid = this.globalService.getselectedStudentId()
     this.getAssignmentList();
-    this.tutornamefordisplay=this.assignmentsService.getselectedtutorname()
+    this.tutornamefordisplay = this.assignmentsService.getselectedtutorname()
   }
-  ionViewDidEnter(){
+  ionViewDidEnter() {
 
     this.platform.backButton.subscribeWithPriority(10, () => {
-      this.router.navigateByUrl('tabs/session-view/session-view/'+this.sessionid)
+      this.router.navigateByUrl('tabs/session-view/session-view/' + this.sessionid)
     });
   }
 
-/*Get AssignmentList*/
+  /*Get AssignmentList*/
 
 
 
-getAssignmentList(){
-  this.sessionstatusended = SessionStatus.SESSION_STATUS_ENDED;
-   // Get the ID that was passed with the URL
-   this.activatedRoute.paramMap.subscribe(params => {
-     let id = params.getAll('id')
-     this.sessionid=id;
-   })
-  this.activatedRoute.paramMap.subscribe(params => {
-    let asgmtid = params.getAll('asgmtid')
-    this.asgmtid=asgmtid;
-  })
+  getAssignmentList() {
+    this.sessionstatusended = SessionStatus.SESSION_STATUS_ENDED;
+    // Get the ID that was passed with the URL
+    this.activatedRoute.paramMap.subscribe(params => {
+      let id = params.getAll('id')
+      this.sessionid = id;
+    })
+    this.activatedRoute.paramMap.subscribe(params => {
+      let asgmtid = params.getAll('asgmtid')
+      this.asgmtid = asgmtid;
+    })
 
-  this.sessionstatus=this.assignmentsService.getselectedsessionstatus();
-  this.storage.get('assignmentdetails').then((val) => {
+    this.sessionstatus = this.assignmentsService.getselectedsessionstatus();
+    this.storage.get('assignmentdetails').then((val) => {
       val.forEach((valor) => {
-      if(this.asgmtid==valor.asgmt_Id)
-      {
-        this.asgmtlist=valor;
-        this.imgdisplayinapp=this.display(valor.asgmt_Data)
-        this.asgmtdiscusslist=this.asgmtlist.assignmentDiscussions;
+        if (this.asgmtid == valor.asgmt_Id) {
+          this.asgmtlist = valor;
+          this.imgdisplayinapp = this.display(valor.asgmt_Data)
+          this.asgmtdiscusslist = this.asgmtlist.assignmentDiscussions;
 
-      }
-    })
-  });
-
-}
-
-
-
-async UpdateData(event)
-{
-  this.sessionstatusended = SessionStatus.SESSION_STATUS_ENDED;
-
-
-  this.token=this.globalService.getselectedtoken()
-  this.studentid=this.globalService.getselectedStudentId()
-  this.sessionstatus=this.assignmentsService.getselectedsessionstatus();
-
-  let header =  {'Accept': 'application/json','Content-Type': 'application/json' }
-  let fetchinfo = {"Authentication_Token":this.token, "Student_Id":this.studentid, "inDepth": "Y"}
-  this.nativeHttp.setDataSerializer('json');
-  this.nativeHttp.post(apiurl.apiUrl+'session/mySessions', fetchinfo,header).then( response => {
-   try {
-     response.data = JSON.parse(response.data);//whole asgmtlist of particular session ,returns a promise
-     this.storage.set('AssignmentDiscussion',response.data).then(val =>{
-      this.storage.get('AssignmentDiscussion').then((val) =>
-      {
-        // console.log('storage get',this.storage.get('AssignmentDiscussion'))
-        val.forEach((valor) => 
-        {
-            this.test = valor.assignments;
-            this.test.forEach(valor2 => 
-          {
-              this.test2 = valor2.asgmt_Id;
-            if(this.asgmtid==this.test2)
-            {
-              this.test = valor2.assignmentDiscussions;
-              this.test3 = valor2 
-              console.log('this is the apilist' , this.test.created_On)
-
-              this.imgdisplayinapp=this.display(this.test3.asgmt_Data)
-              this.asgmtdiscusslist=this.test;
-            }
-          });
-        })
+        }
       })
-    event.target.complete();    
-    })
- } 
- catch(e) {
-  console.error('JSON parsing error');
-  this.assignmentsService.presentError()
-  setTimeout(() => {
-   event.target.complete()
-   this.router.navigateByUrl('tabs/main');  
+    });
 
- }, 3000); 
-}
+  }
 
-}).catch(response => {
-  // prints 403
-  console.log(response.status, ' errorsss');
-  // prints Permission denied
-  console.log(response.error, ' error');
-  this.assignmentsService.presentError()
-  setTimeout(() => {
-   event.target.complete()
-   this.router.navigateByUrl('tabs/main');  
 
- }, 3000);
-  });
 
-}
+  async UpdateData(event) {
+    this.sessionstatusended = SessionStatus.SESSION_STATUS_ENDED;
+
+
+    this.token = this.globalService.getselectedtoken()
+    this.studentid = this.globalService.getselectedStudentId()
+    this.sessionstatus = this.assignmentsService.getselectedsessionstatus();
+
+    let header = { 'Accept': 'application/json', 'Content-Type': 'application/json' }
+    let fetchinfo = { "Authentication_Token": this.token, "Student_Id": this.studentid, "inDepth": "Y" }
+    this.nativeHttp.setDataSerializer('json');
+    this.nativeHttp.post(apiurl.apiUrl + 'session/mySessions', fetchinfo, header).then(response => {
+      try {
+        response.data = JSON.parse(response.data);//whole asgmtlist of particular session ,returns a promise
+        this.storage.set('AssignmentDiscussion', response.data).then(val => {
+          this.storage.get('AssignmentDiscussion').then((val) => {
+            // console.log('storage get',this.storage.get('AssignmentDiscussion'))
+            val.forEach((valor) => {
+              this.test = valor.assignments;
+              this.test.forEach(valor2 => {
+                this.test2 = valor2.asgmt_Id;
+                if (this.asgmtid == this.test2) {
+                  this.test = valor2.assignmentDiscussions;
+                  this.test3 = valor2
+                  console.log('this is the apilist', this.test.created_On)
+
+                  this.imgdisplayinapp = this.display(this.test3.asgmt_Data)
+                  this.asgmtdiscusslist = this.test;
+                }
+              });
+            })
+          })
+          event.target.complete();
+        })
+      }
+      catch (e) {
+        console.error('JSON parsing error');
+        this.assignmentsService.presentError()
+        setTimeout(() => {
+          event.target.complete()
+          this.router.navigateByUrl('tabs/main');
+
+        }, 3000);
+      }
+
+    }).catch(response => {
+      // prints 403
+      console.log(response.status, ' errorsss');
+      // prints Permission denied
+      console.log(response.error, ' error');
+      this.assignmentsService.presentError()
+      setTimeout(() => {
+        event.target.complete()
+        this.router.navigateByUrl('tabs/main');
+
+      }, 3000);
+    });
+
+  }
 
   // refreshing(){
   //   this.ngZone.run(() => {
@@ -160,74 +153,76 @@ async UpdateData(event)
 
 
   /*Segment */
- segmentChanged(event) {
-   if(this.segmentModel!="assignment")
-   {
-   this.isactiveassignment="inactivesegment";
-   this.isactivevideoorvoice="activesegment";
-   }
-   else if (this.segmentModel=="assignment"){
-     this.isactiveassignment="activesegment";
-     this.isactivevideoorvoice="inactivesegment";
-   }
- }
-/*Chat Pop up*/
-async presentModal(ev: any) {
-  const popover = await this.modalController.create({
-    component: SessionAssignmentChatPage,
-    cssClass: 'small-modal',
-    backdropDismiss: true
-  });
-  return await popover.present();
-}
-/*Edit Drawing*/
-EditDrawing(){
-  if(this.networkService.online == false )
-  {
-    this.networkService.presentNetworkMsg(); 
-  }
-  else{
-    this.assignmentsService.presentDrawing('Cancel','Confirm').then((res) => {
-      if (res === 'Confirm') {
-        this.router.navigateByUrl('/tabs/session-view/session-assignment-edit/'+this.sessionid+'/'+this.asgmtid);   
-        //Drawing Page
-      }
-     });
-  }
-  }
-   /*Open Chat*/
-StayonPage(asgmtid)
-{
-  if(this.networkService.online == false )
-  {
-    this.networkService.presentNetworkMsg(); 
-  }
-  else{
-    this.assignmentsService.setselectedasgmtid(asgmtid);
-    this.presentModal(event);
-  }
-}
-  PreviousPage(){
-    if(this.networkService.online == false )
-    {
-      this.networkService.presentNetworkMsg(); 
+  segmentChanged(event) {
+    if (this.segmentModel == "conferencing") {
+      this.isactiveassignment = "inactivesegment";
+      this.isactivevideoorvoice = "activesegment";
+      this.isactiveanalysis = "inactivesegment";
     }
-    else{
-    this.router.navigateByUrl('/tabs/session-view/session-view/'+this.sessionid);
-    }  
+    else if (this.segmentModel == "assignment") {
+      this.isactiveassignment = "activesegment";
+      this.isactivevideoorvoice = "inactivesegment";
+      this.isactiveanalysis = "inactivesegment";
+    }
+    else if (this.segmentModel == "analysis") {
+      // console.log("analysis tab")
+      this.isactiveassignment = "inactivesegment";
+      this.isactivevideoorvoice = "inactivesegment";
+      this.isactiveanalysis = "activesegment";
+    }
+  }
+  /*Chat Pop up*/
+  async presentModal(ev: any) {
+    const popover = await this.modalController.create({
+      component: SessionAssignmentChatPage,
+      cssClass: 'small-modal',
+      backdropDismiss: true
+    });
+    return await popover.present();
+  }
+  /*Edit Drawing*/
+  EditDrawing() {
+    if (this.networkService.online == false) {
+      this.networkService.presentNetworkMsg();
+    }
+    else {
+      this.assignmentsService.presentDrawing('Cancel', 'Confirm').then((res) => {
+        if (res === 'Confirm') {
+          this.router.navigateByUrl('/tabs/session-view/session-assignment-edit/' + this.sessionid + '/' + this.asgmtid);
+          //Drawing Page
+        }
+      });
+    }
+  }
+  /*Open Chat*/
+  StayonPage(asgmtid) {
+    if (this.networkService.online == false) {
+      this.networkService.presentNetworkMsg();
+    }
+    else {
+      this.assignmentsService.setselectedasgmtid(asgmtid);
+      this.presentModal(event);
+    }
+  }
+  PreviousPage() {
+    if (this.networkService.online == false) {
+      this.networkService.presentNetworkMsg();
+    }
+    else {
+      this.router.navigateByUrl('/tabs/session-view/session-view/' + this.sessionid);
+    }
   }
   //Image Sanitize
-  display(bImg:string)
-  {
-    return this.dms.bypassSecurityTrustResourceUrl("data:image/png;base64, "+bImg)
+  display(bImg: string) {
+    return this.dms.bypassSecurityTrustResourceUrl("data:image/png;base64, " + bImg)
   }
   //Display Image 
   async presentModalImage(imgdisplayinapp) {
     const modal = await this.modalController.create({
       component: SessionViewImageDiscussionPage,
-      componentProps: { 
+      componentProps: {
         imagedisplay: this.imgdisplayinapp,
-        discussimgdisplayinapp:this.asgmtlist.asgmtDiscuss_Data
+        discussimgdisplayinapp: this.asgmtlist.asgmtDiscuss_Data
       }
     });
     return await modal.present();
@@ -235,7 +230,7 @@ StayonPage(asgmtid)
   async presentModalDiscussImage(imgdisplayinapp) {
     const modal = await this.modalController.create({
       component: SessionViewImageDiscussionPage,
-      componentProps: { 
+      componentProps: {
         imagedisplay: this.display(imgdisplayinapp),
       }
     });

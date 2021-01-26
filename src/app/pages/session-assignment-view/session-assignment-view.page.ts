43dab1f0da332d2d02b6c5567fca2ada563adceb
Azim212 <44842575+Azim212@users.noreply.github.com>
2021-01-26 16:12:53 +0800

    custom tooltip ui to accomodate pic of asgmtDisc

--- src/app/pages/session-assignment-view/session-assignment-view.page.ts
+++ src/app/pages/session-assignment-view/session-assignment-view.page.ts
@@ -296,22 +296,106 @@ export class SessionAssignmentViewPage implements OnInit {
         data: {
           labels: xAxisLabels,
           datasets: [{
-            label: "Boredom",
+            label: "Bored",
             data: boredDurData,
-            backgroundColor: 'rgb(212, 168, 110)',
+            backgroundColor: 'rgb(26, 199, 230)',
           },
           {
-            label: "Frustration",
+            label: "Frustrated",
             data: frusDurData,
-            backgroundColor: 'rgb(255, 105, 105)',
+            backgroundColor: 'rgb(25, 170, 222)',
           },
           {
             label: "Total",
             data: totalDurData,
-            backgroundColor: 'rgb(69, 69, 69)',
+            backgroundColor: 'rgb(23, 107, 160)',
           }]
         },
         options: {
+          tooltips: {
+            enabled: false,
+            custom: function (tooltipModel) {
+              // Tooltip Element
+              var tooltipEl = document.getElementById('chartjs-tooltip');
+
+              // Create element on first render
+              if (!tooltipEl) {
+                tooltipEl = document.createElement('div');
+                tooltipEl.id = 'chartjs-tooltip';
+                tooltipEl.style.background = "rgba(0, 0, 0, .7)";
+                tooltipEl.style.color = "white";
+                tooltipEl.style.borderRadius = "3px";
+                tooltipEl.style.transition = "opacity .1s ease";
+                tooltipEl.style.transform = "translate(-50%, 0)";
+                document.body.appendChild(tooltipEl);
+              }
+
+              // Hide if no tooltip
+              if (tooltipModel.opacity === 0) {
+                tooltipEl.style.opacity = "0";
+                return;
+              }
+
+              // Set caret Position
+              tooltipEl.classList.remove('above', 'below', 'no-transform');
+              if (tooltipModel.yAlign) {
+                console.log(tooltipModel.yAlign)
+                tooltipEl.classList.add(tooltipModel.yAlign);
+              } else {
+                tooltipEl.classList.add('no-transform');
+              }
+
+              function getBody(bodyItem) {
+                return bodyItem.lines;
+              }
+
+              // Set Text
+              if (tooltipModel.body) {
+                var titleLines = tooltipModel.title || [];
+                var bodyLines = tooltipModel.body.map(getBody);
+
+                // img of asgmtDisc
+                var imgSrc = "https://images.theconversation.com/files/93616/original/image-20150902-6700-t2axrz.jpg?ixlib=rb-1.1.0&q=45&auto=format&w=1000&fit=clip"
+                var innerHtml = '<img src="' + imgSrc + '" alt="google" width="150" height="150"></img>'
+
+                innerHtml += '<table><thead>';
+
+                titleLines.forEach(function (title) {
+                  innerHtml += '<tr><th>' + title + '</th></tr>';
+                });
+                innerHtml += '</thead><tbody>';
+
+
+                bodyLines.forEach(function (body, i) {
+                  var colors = tooltipModel.labelColors[i];
+                  console.log(tooltipModel.labelColors)
+                  var style = 'background:' + colors.backgroundColor;
+                  style += '; border-color:' + colors.borderColor;
+                  style += '; border-width: 2px'
+                    + ";display: inline-block;width: 10px;height: 10px;margin-right: 10px;"
+                  var span = '<span style="' + style + '"></span>';
+                  innerHtml += '<tr><td>' + span + body + '</td></tr>';
+                });
+                innerHtml += '</tbody></table>';
+
+                tooltipEl.innerHTML = innerHtml;
+              }
+
+              // `this` will be the overall tooltip
+              var position = ctx.getBoundingClientRect();
+
+              // Display, position, and set styles for font
+              tooltipEl.style.opacity = "1";
+              tooltipEl.style.position = 'absolute';
+              tooltipEl.style.left = position.left + window.pageXOffset + tooltipModel.caretX + 'px';
+              tooltipEl.style.top = position.top + window.pageYOffset + tooltipModel.caretY + 'px';
+              tooltipEl.style.fontFamily = tooltipModel._bodyFontFamily;
+              tooltipEl.style.fontSize = tooltipModel.bodyFontSize + 'px';
+              tooltipEl.style.fontStyle = tooltipModel._bodyFontStyle;
+              tooltipEl.style.padding = tooltipModel.yPadding + 'px ' + tooltipModel.xPadding + 'px';
+              tooltipEl.style.pointerEvents = 'none';
+            }
+          },
           scales: {
             yAxes: [{
               ticks: {
@@ -348,5 +432,4 @@ export class SessionAssignmentViewPage implements OnInit {
     //   }).then(resp => resp.json())
     // ))
   }
-
 }
